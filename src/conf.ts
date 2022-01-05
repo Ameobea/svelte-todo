525c4cff5e24e0f38c15bf9bd1e9787d5c5b3eed
Casey Primozic <me@ameo.link>
2022-01-05 01:39:48 -0600

    Fix env parsing

--- src/conf.ts
+++ src/conf.ts
@@ -1,10 +1,10 @@
-export const ExpectedBasicAuthValue = process.env['HTTP_BASIC_AUTH_EXPECTED_VALUE'];
-export const SQLiteDbFilePath = process.env['SQLITE_DB_FILE_PATH'];
+export const ExpectedBasicAuthValue = import.meta.env['VITE_HTTP_BASIC_AUTH_EXPECTED_VALUE'];
+export const SQLiteDbFilePath = import.meta.env['VITE_SQLITE_DB_FILE_PATH'];
 
 if (!ExpectedBasicAuthValue) {
-  console.error('Missing expected `HTTP_BASIC_AUTH_EXPECTED_VALUE` environment variable');
+  console.error('Missing expected `VITE_HTTP_BASIC_AUTH_EXPECTED_VALUE` environment variable');
   process.exit(1);
 } else if (!SQLiteDbFilePath) {
-  console.error('Missing expected `SQLITE_DB_FILE_PATH` environment variable');
+  console.error('Missing expected `VITE_SQLITE_DB_FILE_PATH` environment variable');
   process.exit(1);
 }
